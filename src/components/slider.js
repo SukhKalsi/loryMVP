4ccb0d87065a71b7d610ad62adcd07436ee7ba4e
Sukh Kalsi <SukhKalsi@users.noreply.github.com>
2016-10-26 14:41:36 +0100

    experimenting with lory events and positioning slides

--- src/components/slider.js
+++ src/components/slider.js
@@ -1,37 +1,112 @@
 import {lory} from 'lory.js';
+require('./slider.scss');
 
 class Slider {
   
   constructor() {
-    const sliders = document.querySelectorAll('.js_slider');
+    // const sliders = document.querySelectorAll('.js_slider');
 
-    for (var slider of sliders) {
+    // for (var slider of sliders) {
+    //   this.bindEvents(slider);
+    //   lory(slider, {
+    //       infinite: 1,
+    //       enableMouseEvents: true
+    //   });
+    // }
 
-      this.bindEvents(slider);
-      lory(slider, {
-          infinite: 1,
-          enableMouseEvents: true
-      });
-    }
+    this.slider = document.querySelector('.js_slider');
+    // this.resizeSlides();
+    this.bindEvents();
+    this.init();
+
+    console.log('setup complete.');
+
+    // setTimeout(function() {
+    //   carousel.destroy();
+    //   console.log('carousel should be destroyed');
+    //   console.log(carousel);
+      
+    //   setTimeout(function() {
+    //     carousel.setup();
+    //     console.log('carousel re setup.');
+    //   }, 10000);
+
+    // }, 10000);
   }
 
-  bindEvents(slider) {
-    slider.addEventListener('before.lory.init', this.handleEvent.bind(this));
-    slider.addEventListener('after.lory.init', this.handleEvent.bind(this));
-    slider.addEventListener('before.lory.slide', this.handleEvent.bind(this));
-    slider.addEventListener('after.lory.slide', this.handleEvent.bind(this));
+  init() {
+    const carousel = lory(this.slider, {
+      enableMouseEvents: true,
+    });
+    this.carousel = carousel;
+  }
 
-    slider.addEventListener('on.lory.resize', this.handleEvent.bind(this));
-    slider.addEventListener('on.lory.touchend', this.handleEvent.bind(this));
-    slider.addEventListener('on.lory.touchmove', this.handleEvent.bind(this));
-    slider.addEventListener('on.lory.touchstart', this.handleEvent.bind(this));
-    slider.addEventListener('on.lory.destroy', this.handleEvent.bind(this));
+  bindEvents() {
+    this.slider.addEventListener('before.lory.init', this.handleEvent.bind(this));
+    this.slider.addEventListener('after.lory.init', this.handleEvent.bind(this));
+    // this.slider.addEventListener('before.lory.slide', this.handleEvent.bind(this));
+    // this.slider.addEventListener('after.lory.slide', this.handleEvent.bind(this));
+
+    this.slider.addEventListener('on.lory.touchend', this.handleEvent.bind(this));
+    // slider.addEventListener('on.lory.resize', this.handleEvent.bind(this));
+    // slider.addEventListener('on.lory.touchmove', this.handleEvent.bind(this));
+    // slider.addEventListener('on.lory.touchstart', this.handleEvent.bind(this));
+    // slider.addEventListener('on.lory.destroy', this.handleEvent.bind(this));
   }
 
   handleEvent(e) {
-      var time = new Date();
-      time = time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds() + ',' + time.getMilliseconds();
-      console.log('[' + time + '] Event dispatched: "' + e.type + '"');
+    const slides = this.slider.querySelectorAll('.js_slide');
+    const track = this.slider.querySelector('.js_slides');
+    const peek = Math.floor(this.slider.offsetWidth * 0.2); 
+    const width = this.slider.offsetWidth - peek;
+    const padding = peek / 2;
+    let transform;
+
+    console.log('handleEvent', e.type);
+
+    switch (e.type) {
+      case 'before.lory.init':
+        for (let i = 0; i < slides.length; i++) {
+          slides[i].style.width = `${width}px`;
+        }
+        break;
+        
+      case 'after.lory.init':
+        transform = `translate3d(${padding}px, 0, 0)`;
+        this.transform(track, transform);
+        break;
+
+      case 'on.lory.touchend':
+        console.log(this.carousel.returnIndex());
+        console.log(track)
+
+        if (this.carousel.returnIndex() === 0) {
+          transform = `translate3d(${padding}px, 0, 0)`;
+          this.transform(track, transform);
+        } else {
+
+          setTimeout(() => {
+            const xPos = track.getBoundingClientRect().left - track.offsetLeft - track.parentNode.offsetLeft;
+            const isLastSlide = Math.abs(xPos) > (width * this.carousel.returnIndex());
+            let translateX = xPos + padding;
+
+            if (isLastSlide) {
+              translateX = (width * (slides.length - 1) - padding) * -1;
+            }
+
+            transform = `translate3d(${translateX}px, 0, 0)`;
+            this.transform(track, transform);
+
+          }, 300);
+        }
+        break;
+    }
+  }
+
+  transform(el, value) {
+    el.style.webkitTransform = value;
+    el.style.mozTransform = value;
+    el.style.transform = value;
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
   entry  : __dirname + '/src/index.js',
   output : {
@@ -10,7 +12,15 @@ module.exports = {
         test   : /.js$/,
         exclude: /node_modules/,
         loader : 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract('style-loader', ['css-loader', 'postcss-loader', 'sass-loader'])
       }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin("styles.css")
+  ]
 };
